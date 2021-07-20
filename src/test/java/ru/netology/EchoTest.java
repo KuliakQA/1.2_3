9e74d8fcdb8ba42285f185c23aeafcc0db28451b
KuliakQA <culeacvadim@gmail.com>
2021-07-20 18:47:30 +0200

    ok

--- src/test/java/ru/netology/EchoTest.java
+++ src/test/java/ru/netology/EchoTest.java
@@ -17,7 +17,7 @@ public class EchoTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
